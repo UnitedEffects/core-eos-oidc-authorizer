59e7b6f88537f95795dfb1588ef41ad4240dced5
Bo Motlagh <borzou@theboeffect.com>
2022-05-19 17:37:07 -0400

    updating vars

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const env = process.env.NODE_ENV || 'dev';
 const dir = (fs.existsSync('./.env')) ? '.env' : '.env_sample';
-const envVars = require(`../${dir}/.env.${env}`);
+const envVars = require(`../${dir}/env.${env}`);
 
 const config = {
     ENV: process.env.NODE_ENV || envVars.NODE_ENV || 'dev',
     JWKS_URI: process.env.JWKS_URI || envVars.JWKS_URI || 'https://auth.unitedeffects.com/YOURAUTHGROUP/jwks',
     CORE_URI: process.env.CORE_URI || envVars.CORE_URI || 'https://auth.unitedeffects.com',
     AUTH_GROUP: process.env.AUTH_GROUP || envVars.AUTH_GROUP || 'YOURAUTHGROUP',
-    AUD: process.env.AUD || envVars.AUD || null,
-    PKCE: process.env.PKCE || envVars.PKCE || false,
+    AUD: (process.env.AUD !== "") ? process.env.AUD : (envVars.AUD !== "") ? envVars.AUD : null,
+    PKCE: (process.env.PKCE === 'true') || envVars.PKCE || false,
     CLIENT_ID: process.env.CLIENT_ID || envVars.CLIENT_ID || null,
-    CLIENT_SECRET: process.env.CLIENT_SECRET || envVars.CLIENT_SECRET || null,
-    GET_USER: process.env.GET_USER || envVars.GET_USER || false,
-    INCLUDE_CONTEXT: process.env.INCLUDE_CONTEXT || envVars.INCLUDE_CONTEXT || true,
+    CLIENT_SECRET: (process.env.CLIENT_SECRET !== "") ? process.env.CLIENT_SECRET : (envVars.CLIENT_SECRET !== "") ? envVars.CLIENT_SECRET : null,
+    GET_USER: (process.env.GET_USER === 'true') || envVars.GET_USER || false,
+    INCLUDE_CONTEXT: (process.env.INCLUDE_CONTEXT === 'true') || envVars.INCLUDE_CONTEXT || true,
 };
 
 module.exports = config;
